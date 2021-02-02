5a469b8339be8663c74d732eb2d264307c21a439
Andrew Reiblein <andrewmreiblein013@gmail.com>
2021-02-02 17:25:57 +0200

    third commit

--- NewsApp/urls.py
+++ NewsApp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, bbc
+from .views import index, bbc, techcrunch
 
 urlpatterns = [
     path('', index, name = "index"),
-    path('bbc/', bbc, name = 'BBC')
+    path('bbc/', bbc, name = 'BBC'),
+    path('techcrunch/', techcrunch, name = 'techcrunch')
     
 
 
--- NewsApp/views.py
+++ NewsApp/views.py
@@ -50,3 +50,29 @@ def bbc(request):
 
 
     return render(request, 'bbc.html', context={"mylist":mylist})
+
+
+def techcrunch(request):
+    newsapi = NewsApiClient(api_key="cfa38c8065ed4b039cb827db8924d7ee")
+    topheadlines = newsapi.get_top_headlines(sources='techcrunch')
+
+    articles = topheadlines['articles']
+
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+
+
+    mylist = zip(news, desc, img)
+
+
+    return render(request, 'tech.html', context={"mylist":mylist})
+
+    
